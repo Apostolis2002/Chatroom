c9400472aabf97845fd60fa0f82c927960138896
ApostolisSiampanis <apostolissiabanis@hotmail.com>
2023-12-28 16:15:26 +0200

    Initial Commit

---
+++ app/src/main/java/com/example/chatroom/MainActivity2.java
@@ -0,0 +1,62 @@
+package com.example.chatroom;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class MainActivity2 extends AppCompatActivity {
+    TextView textView2,allMeessages;
+    EditText message;
+    String nickname;
+    FirebaseDatabase database;
+    DatabaseReference reference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        textView2 = findViewById(R.id.textView2);
+        nickname = getIntent().getStringExtra("nickname");
+        textView2.setText("Hello "+nickname);
+        allMeessages = findViewById(R.id.textView3);
+        allMeessages.setText("");
+        message = findViewById(R.id.editTextText3);
+        database = FirebaseDatabase.getInstance();
+        reference = database.getReference("message");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String previousMessages = allMeessages.getText().toString();
+                if (snapshot.getValue()!=null)
+                    allMeessages.setText(previousMessages+"\n"+snapshot.getValue().toString());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+    public void send(View view){
+        if (!message.getText().toString().trim().isEmpty()){
+            reference.setValue(nickname+":"+message.getText().toString());
+            message.setText("");
+        }else {
+            showMessage("Error","Please write a message first!..");
+        }
+    }
+    void showMessage(String title, String message){
+        new AlertDialog.Builder(this).setTitle(title).setMessage(message).setCancelable(true).show();
+    }
+}
